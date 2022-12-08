1cc1d17a9bf72aab8a42db7c90c4bed966aac50e
HexnDex <eowusularbi@gmail.com>
2022-12-08 10:09:23 -0800

    C Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Program must print out a line using puts
+ *
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")';
+	return(0);

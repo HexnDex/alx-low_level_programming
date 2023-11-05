89c41271dc410466f77567556ed70445fd7c0393
HexnDex <eowusularbi@gmail.com>
2023-11-05 11:03:26 -0800

    adding task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry to the program
+ * @argc: number of command line args
+ * @argv: array of command line args
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

2d5d4aeab11a9af9dada263f2cebaad3e5cbb869
HexnDex <eowusularbi@gmail.com>
2023-11-06 02:58:03 -0800

    adding task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_array - fxn to create array of chars
+ *and initializes to specified character
+ * @size: size of array
+ * @c: character to initialize in array
+ *
+ * Return: returns NULL if size is 0
+ *OR fails and pointer to array if successfull
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = 'c';
+	}
+
+	return (array);
+
+}
